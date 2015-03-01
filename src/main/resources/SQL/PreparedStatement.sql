45a64b2170df09df85cf41a9f9781a31ecc0494e
Iusinda <lusindaleung@gmail.com>
2015-03-02 02:00:28 +0800

    Added a file storing all prepared statements

---
+++ src/main/resources/SQL/PreparedStatement.sql
@@ -0,0 +1,79 @@
+--Customer
+
+--C first-time use
+INSERT INTO rms.Customers (RegID) VALUES ('?');
+SELECT LAST_INSERT_ID();
+
+--S call customer
+SELECT RegID FROM rms.Customers WHERE ID = ?;
+
+----------
+
+--Area
+
+--C list areas
+SELECT * FROM rms.Areas;#
+
+--District
+
+--C list districts
+SELECT ID, Name FROM rms.Districts WHERE AreaID = ?;
+
+----------
+
+--Restaurant
+
+--C list restaurants
+SELECT ID, Name FROM rms.Restaurants WHERE Availability = TRUE;
+
+--C search and/or filter restaurants
+--filter nothing
+SELECT ID, Name FROM rms.Restaurants WHERE Availability = TRUE AND Name LIKE '%?%';
+--filter Area only
+SELECT ID, Name FROM rms.Restaurants WHERE Availability = TRUE AND DistrictID IN (SELECT ID FROM rms.Districts WHERE AreaID = ?) AND Name LIKE '%?%';
+--filter Area and District
+SELECT ID, Name FROM rms.Restaurants WHERE Availability = TRUE AND DistrictID = ? AND Name LIKE '%?%';
+
+--C choose and view a restaurant
+--S show info before modifying
+SELECT * FROM rms.Restaurants WHERE ID = ?;
+
+--S login
+SELECT Availability FROM rms.Restaurants WHERE ID = ? AND Password = '?';
+
+--S turn on waiting list
+UPDATE rms.Restaurants SET Availability = TRUE WHERE ID = ?;
+
+--S turn off waiting list
+SELECT LastUpdate FROM rms.Restaurants WHERE ID = ?;
+UPDATE rms.Restaurants SET Availability = FALSE, LastUpdate = NOW(6) WHERE ID = ?;
+
+--S modify restaurant info
+UPDATE rms.Restaurants SET Name = '?', DistrictID = ?, Address = '?', PhoneNo = '?', OpeningHours = '?', Description = '?' WHERE ID = ?;
+
+--S change password
+UPDATE rms.Restaurants SET Password = '?' WHERE ID = ? AND Password = '?';
+
+----------
+
+--Ticket
+
+--C create ticket
+INSERT INTO rms.Tickets (RestaurantID, Type, Size, CustomerID) VALUES (?, '?', ?, ?);
+--S create ticket
+INSERT INTO rms.Tickets (RestaurantID, Type, Size) VALUES (?, '?', ?);
+
+--S show waiting list
+SELECT CustomerID, Size, GetTime, CallTime FROM rms.Tickets WHERE RestaurantID = ? AND Type = '?' AND Validity = TRUE ORDER BY GetTime;
+
+--C view ticket
+SELECT COUNT(*) FROM rms.Tickets WHERE RestaurantID = ? AND Type = '?' AND GetTime < '?' AND Validity = TRUE AND CallTime IS NULL;
+
+--S call ticket
+UPDATE rms.Tickets SET CallTime = NOW() WHERE RestaurantID = ? AND Type = '?' AND GetTime = '?';
+
+--S remove ticket
+UPDATE rms.Tickets SET Validity = FALSE WHERE RestaurantID = ? AND Type = '?' AND GetTime = '?';
+
+--S turn off waiting list: record tickets of each type
+SELECT GetTime, CallTime FROM rms.Tickets WHERE RestaurantID = ? AND Type = '?' AND CallTime > (SELECT LastUpdate FROM rms.Restaurants WHERE ID = ?);
